39d14476d20c211a4185b656fcde4450a9e14d1a
Rushikesh Tushar patil <hrushikeshp684@gmail.com>
2025-11-30 18:10:30 +0530

    Update SEO meta tags

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -148,7 +148,7 @@ const Hero = () => {
 
       {/* Scroll Indicator */}
       <motion.div
-        className="absolute bottom-8 left-1/2 transform -translate-x-1/2"
+        className="absolute bottom-8 right-8"
         animate={{
           y: [0, 10, 0],
         }}
